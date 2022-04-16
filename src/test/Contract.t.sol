dcf4ce0490071a268455adaa6bdc2dc5156a91f4
K-Aizen <k.aizen.crypto.dev@gmail.com>
2022-04-16 15:01:05 +0000

    Improving Test

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -2,11 +2,16 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "forge-std/console.sol";
 
 contract ContractTest is DSTest {
     function setUp() public {}
 
     function testExample() public {
+        uint256 test = 5656565656;
+        console.log("The tx.origin is ", tx.origin);
+        console.log("The msg.sender is ", msg.sender);
+        console.log("The test is ", test + 1);
         assertTrue(true);
     }
 }
