a3a28b37cab5a72cd48cb36fc35b88d6e768c033
k-aizen <k.aizen.crypto.dev@gmail.com>
2022-05-03 09:22:26 +0000

    Adding the Perp supported collateral addresses on Optimism Kovan1

--- src/Contract.sol
+++ src/Contract.sol
@@ -15,7 +15,7 @@ import {WETH10} from "@weth10/WETH10.sol";
 import {TransferHelper} from '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import {LemmaSwap} from "./LemmaSwap/LemmaSwap.sol";
 // import {ERC20} from "solmate/tokens/ERC20.sol";
-
+import "forge-std/Test.sol";
 
 import "forge-std/console.sol";
 
@@ -35,7 +35,7 @@ contract Collateral is ERC20 {
 //     }
 // }
 
-contract Deployment {
+contract Deployment is Test {
     MockOracle public oracle;
     MockPerp public perp;
     // Collateral public weth;
@@ -53,6 +53,8 @@ contract Deployment {
 
     struct s_testnet {
         address WETH;
+        address WBTC;
+        address USDC;
         address USDLemma;
     }
 
@@ -61,8 +63,19 @@ contract Deployment {
     s_testnet public testnet_optimism_kovan;
 
     constructor() {
+        // https://github.com/lemma-finance/scripts/blob/312f7c9f45186610e98396693c81a26ead9e0a6e/config.json#L45
         testnet_optimism_kovan.WETH = address(0x4200000000000000000000000000000000000006);
+
+        // https://github.com/lemma-finance/scripts/blob/312f7c9f45186610e98396693c81a26ead9e0a6e/config.json#L49
+        testnet_optimism_kovan.WBTC = address(0xf69460072321ed663Ad8E69Bc15771A57D18522d);
+
+        // https://github.com/lemma-finance/scripts/blob/312f7c9f45186610e98396693c81a26ead9e0a6e/config.json#L41
+        testnet_optimism_kovan.USDC = address(0x3e22e37Cb472c872B5dE121134cFD1B57Ef06560);
+
+        // https://github.com/lemma-finance/scripts/blob/312f7c9f45186610e98396693c81a26ead9e0a6e/config.json#L307
         testnet_optimism_kovan.USDLemma = address(0x15264ce29dEccc0C997D2Bd9D2accBBe37306517);
+
+
         useRealUSDL = true;
     }
 
@@ -100,17 +113,17 @@ contract Deployment {
         // weth.deposit{value: 100e18}();
         // console.log("[deployTestnet()] WETH Balance = ", weth.balanceOf(address(this)));
         // weth = IWETH10(address(new MyWETH(100e18)));
-        wbtc = IERC20(address(new Collateral("WBTC", "WBTC", 100e18)));
-        oracle = new MockOracle();
-        oracle.setPriceNow(address(weth), Denominations.USD, 100e18);
-        oracle.setPriceNow(address(wbtc), Denominations.USD, 120e18);
+        wbtc = IWETH10(testnet.WBTC);
+        deal(address(wbtc), address(this), 10e25);
+        // oracle = new MockOracle();
+        // oracle.setPriceNow(address(weth), Denominations.USD, 100e18);
+        // oracle.setPriceNow(address(wbtc), Denominations.USD, 120e18);
 
-        perp = new MockPerp(
-            IMockOracle(address(oracle)),
-            1000,   // feeOpenShort = 0.1% 
-            1000    // feeCloseShort = 0.1%
-        );
-
+        // perp = new MockPerp(
+        //     IMockOracle(address(oracle)),
+        //     1000,   // feeOpenShort = 0.1% 
+        //     1000    // feeCloseShort = 0.1%
+        // );
 
         lemmaTreasury = new MockLemmaTreasury();
 
